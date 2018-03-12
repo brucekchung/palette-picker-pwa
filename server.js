db76cecf8dce995e494fd73ef40139b3728f085b
Bruce Chung <brucekchung@gmail.com>
2018-03-12 14:24:02 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+  console.log(`listening on 3000`)
+})
+
+// app.get('/', (req, res) => {
+//   res.status(200).send('oh yahyyyyy')
+// })
+
