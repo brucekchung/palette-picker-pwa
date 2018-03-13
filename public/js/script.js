9c7b5fb53b922fe63883132ca0831e21de153a89
Bruce Chung <brucekchung@gmail.com>
2018-03-13 17:56:00 -0600

    Add UI for picking color

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,31 @@
-console.log('im linked!!!')
+window.onload = () => setRandomColor()
+
+$('.generate-palette').on('click', setRandomColor)
+$('.save-project').on('click', saveProject)
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+
+  return color
+}
+
+function setRandomColor() {
+  const allColors = [$('.one'), $('.two'), $('.three'), $('.four'), $('.five')]
+
+  allColors.forEach(color => {
+    const randomColor = getRandomColor()
+    color.css('background-color', randomColor)
+    color.children('span').text(randomColor)
+  })
+}
+
+function saveProject() {
+  console.log('saved??..')
+}
+
+
